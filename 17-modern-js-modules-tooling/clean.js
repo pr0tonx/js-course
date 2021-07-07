4094e604bad16dedb1b67e58d7f064c5c9a5b843
Felipe <felipe.engler@tuntscorp.com>
2021-07-07 01:56:40 -0300

    added chapter seventeen;

---
+++ 17-modern-js-modules-tooling/clean.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const budget = Object.freeze([
+  {value: 250, description: 'Sold old TV 📺', user: 'jonas'},
+  {value: -45, description: 'Groceries 🥑', user: 'jonas'},
+  {value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas'},
+  {value: 300, description: 'Freelancing 👩‍💻', user: 'jonas'},
+  {value: -1100, description: 'New iPhone 📱', user: 'jonas'},
+  {value: -20, description: 'Candy 🍭', user: 'matilda'},
+  {value: -125, description: 'Toys 🚂', user: 'matilda'},
+  {value: -1800, description: 'New Laptop 💻', user: 'jonas'},
+]);
+
+const spendingLimits = Object.freeze({
+  jonas: 1500,
+  matilda: 100,
+});
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// Pure function
+const addExpense = function (state, limits, value, description, user = 'jonas') {
+  const cleanUser = user.toLowerCase();
+  return value <= getLimit(limits, cleanUser) ? [...state, {
+    value: -value,
+    description: description,
+    user: cleanUser
+  }] : state;
+};
+
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(newBudget1, spendingLimits, 110, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+
+const checkExpenses = (state, limits) => {
+  return state.map(entry => entry.value < -getLimit(limits, entry.user) ? {...entry, flag: 'limit'} : entry);
+};
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+
+// Inpure function
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
+  console.log(bigExpenses);
+};
+
+logBigExpenses(finalBudget, 1000);
